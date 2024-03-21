b92bef3ac9b64574233a5711b3623914f18fe4af
drecrash <dredude360@gmail.com>
2024-03-20 17:41:21 -0700

    removed navbar

--- src/pages/components/Nav.tsx
+++ src/pages/components/Nav.tsx
@@ -17,18 +17,18 @@ export default function Nav(){
                     {/* <h1 className="text-lg bottom-0 ml-0 font-bold float-left absolute">
                         <a href="/">dre</a>
                     </h1> */}
-                    <div className="float-left max-w-[10%]">
+                    <div className="float-left max-w-[3%]">
                         <div className="m-1 justify-center items-center space-x-4 flex">
                             <div className="">
                                 <img className="" src="/home_button_navbar.png" alt="dre's pfp" />
                             </div>
-
+{/* 
                             <div className='max-w-[50%] inline-flex gap-x-[10%]'>
 
                                 <a href="/"><img className="pt-1 pb-1" src="/home_icon.png" alt="resume" /></a>
                                 <a href="/resume"><img className="pt-1 pb-1" src="/resume_icon.png" alt="resume" /></a>
                                 <a href="/contact"><img className="pt-1 pb-1" src="/contact_icon.png" alt="contact me" /></a>
-                            </div>
+                            </div> */}
                         </div>
                     </div>
                     <div className='grow border-l border-black mr-3 h-max'></div>
